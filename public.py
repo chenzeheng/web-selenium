cc4b8e2a93d15bf45221827990768ba7732dbf0c
chenzeheng <996845913@qq.com>
2017-04-24 16:51:07 +0800

    Add files via upload

--- public.py
+++ public.py
@@ -27,21 +27,48 @@ class LoginTest(unittest.TestCase):
         driver.find_element_by_link_text("安全退出").click()
         time.sleep(2)
         driver.quit()
-    #登录测试用例1
+    #登录-用户名、密码正确登录
     def test_login1(self):
-        '''用户名、密码正确登录'''
+        '''用户名,密码正确登录'''
         username=Data[0][0]
         password=Data[0][1]
+        self.user_login(self.driver,username,password)
+        time.sleep(2)
+        self.driver.quit()
+    #用户名正确、密码为空登录
+    def test_login2(self):
+        '''用户名正确,密码为空登录'''
+        username=Data[1][0]
+        password=Data[1][1]
+        self.user_login(self.driver,username,password)
+        time.sleep(2)
+        self.driver.quit()
+    #用户名正确，密码错误登录
+    def test_login3(self):
+        '''用户名正确,密码错误登录'''
+        username=Data[2][0]
+        password=Data[2][1]
+        self.user_login(self.driver,username,password)
+        time.sleep(2)
+        self.driver.quit()
+    #用户名错误，密码错误登录
+    def test_login4(self):
+        '''用户名错误,密码错误登录'''
+        username=Data[3][0]
+        password=Data[3][1]
         self.user_login(self.driver,username,password)
         time.sleep(2)
         self.driver.quit()
 if __name__ == '__main__':
     #unittest.main()
     testreport = unittest.TestSuite()
-    testreport.addTest(LoginTest("test_login1"))
+    testreport.addTest(LoginTest("test_login1"))#可以让测试用例按照指定的顺序执行
+    testreport.addTest(LoginTest("test_login2"))
+    testreport.addTest(LoginTest("test_login3"))
+    testreport.addTest(LoginTest("test_login4"))
     path="C:\Users\Public\Desktop" #定义报告路径
     os.chdir(path)#更换当前工作路径为：path("C:\Users\Public\Desktop")
     ReportName='Result_AutoTest_'+time.strftime('%Y-%m-%d-%H-%M-%S',time.localtime(time.time()))+'.html'
     fp=open(ReportName,'wb')
-    runner=HTMLTestRunner.HTMLTestRunner(stream=fp,title="Test Report",description="Test Result Detail")
+    runner=HTMLTestRunner.HTMLTestRunner(stream=fp,title=u"小狗钱钱登录测试报告",description=u"小狗钱钱登录测试详情")
     runner.run(testreport)
